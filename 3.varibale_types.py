995d99901275642498fa00d6a39d961b92299d74
heysushil <sushil.chaudhary111@gmail.com>
2020-08-31 10:53:17 +0530

    varaibels work

---
+++ 3.varibale_types.py
@@ -0,0 +1,23 @@
+'''
+Variable ko declare karne ke tarike:
+
+1. Variable kabhi bhi number se start nahi hoga.
+2. Variable ke beech me spcae nahi hoga.
+3. Varaibel me special charater (!@#$%^) ke naam par underscore (_) use hoga.
+4. Variable name hamesa unique hota hai. Means, small a aur capital A, ye dono alag-alag varaibles hain.
+5. Because alphabets small aur capital letter ke alag hote hain aur inke ASCII code number bhi alag hote hain.
+'''
+
+userfirstname = 'Hey Sushil'
+user_first_name = 'Hey Sushil'
+userFirstName = 'Hey Sushil'
+UserFirstName = 'Hey Sushil'
+myFirstName = user_first_name
+first_name = 'Hey'
+middle_name = 'Sushil'
+last-name = 'Hey'
+
+# 1a = 1
+# a123 = 1
+# a_num = 1
+# a-num = 1
