6c7fcb7be94f196f8e590de4fd209808e26a718f
heysushil <sushil.chaudhary111@gmail.com>
2020-08-31 10:31:52 +0530

    variables

---
+++ 2.variable.py
@@ -0,0 +1,31 @@
+'''
+Python Variables:
+
+
+
+PHP Code Example for comapring with Python code:
+
+$a = 10;
+
+C code example: 
+
+int a = 10;
+
+Kuch datatype:
+
+1. int: simple number with negative or positive
+2. float: accept point numbers
+'''
+
+# first variable in python
+# yaha par a jo hai ye ek varaible hai. new vairable isi tarah se define karte hain
+# 10 jo hai ye ek value hai. jo ki a varaible ke andar store hai.
+# yaha par python me variable ka datatype automatic python samjh jata hain.
+a = 10
+message = '\na varaibel ka type hai: '
+# check type of a using type() in print() method
+print(message, type(a))
+
+
+
+
