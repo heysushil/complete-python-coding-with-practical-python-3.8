2a2f23af04997fc7e678a0addafc0697cde154fe
heysushil <sushil.chaudhary111@gmail.com>
2020-10-05 17:25:57 +0530

    class file updated

--- 18.class.py
+++ 18.class.py
@@ -11,25 +11,35 @@ Note: Class ka first character hamesa capital letter me hona cahiye.
 
 # create class
 class MyClass:
-    name = 'Hello Python'
+    # name = 'Hello Python'
+    # creating constructer
+    def __init__(self, name, mobile):
+        self.name = name
+        self.mobile = mobile
+    
+    def mydetail(self):
+        print('\n' + self.name + ' and ' + self.mobile)
 
-    # sum funciton
-    def mysum(num1, num2):
-        myresult = num1 + num2
-        return myresult
+    def mypersionaldetail(self, email):
+        print('''
+        Hello {}, how are you?
+        Is this your mobile number {}
+        Is this your email id {}
+        '''.format(self.name, self.mobile, email))
 
-# create object
-myobj = MyClass
+# create objecte
+# myclass()
+myobj = MyClass('Neha','987987987')
 
-myobj.name
-# call mysum
-result = myobj.mysum(400, 600)
-print('\nMysum: ', result)
+myobj.mydetail()
+myobj.mypersionaldetail('python@gmail.com')
+# print(myobj.name)
 
-print('\n', type(myobj.name))
 
 '''
 Questions:
 
 1. jaise sum ka funciton bayan hai waise hi aap ek Calculater name ka class banaoge aur usme sum, subtract, multiplication aur divistion ke methods banae hai. Aur ye 4 methods char alag-alag fucntion behaviour ka use karke banae hain.
+
+2. ek calculate name ka class bana hai aur isme number of methods banae hain: addition, subtraction, division, multiplication.
 '''
