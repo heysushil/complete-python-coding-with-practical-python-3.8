498936c33f13780ebb4d69e1ecb4743933d53ff6
heysushil <sushil.chaudhary111@gmail.com>
2020-10-08 18:00:24 +0530

    pip package

---
+++ 23.math_libaray.py
@@ -0,0 +1,21 @@
+'''
+Library kya hai?
+
+1. Library classes aur methods ka collection hai.
+'''
+
+# import math library
+import math as m
+
+print('\nPi: ',m.pi)
+
+print('\nCos: ', m.cos(0.30))
+
+print('\nLog: ', m.log(10))
+
+
+'''
+Program:
+
+1. Math module ke most of well know method ka use karna hai.
+'''
---
+++ 24.pip.py
@@ -0,0 +1,19 @@
+'''
+PIP Packages:
+
+Commands for pip packages:
+
+1. check version: pip --version
+2. check list of existing librarys: pip list
+3. install camelcase library: pip install camelcase
+4. uninstall camelcase libray: pip uninstall camelcase
+'''
+
+# import camelcase as c
+
+# myobj = c.CamelCase()
+
+# message = 'uninstall camel case libray: pip uninstall camel case'
+
+# print(myobj.hump(message))
+
